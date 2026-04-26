abf158862008310092f869f92b4e28cba2cca20a
Yxz12-ye <3078961710@qq.com>
2026-04-26 21:42:15 +0800

    修复ICU4X导致的严重卡顿

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "app-window.h"
 #include "app_controller.h"
 
+#include <cstdlib>
+
 int main()
 {
+#ifdef _WIN32
+    _putenv_s("SLINT_BACKEND", "winit-software");
+    _putenv_s("SLINT_SOFTWARE_RENDERER_PARLEY_DISABLED", "1");
+#else
+    setenv("SLINT_BACKEND", "winit-software", 1);
+    setenv("SLINT_SOFTWARE_RENDERER_PARLEY_DISABLED", "1", 1);
+#endif
+
     auto window = AppWindow::create();
     AppController controller(window);
     controller.initialize();
